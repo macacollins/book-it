c6cef0e1c4ffe8b806ebaeb8bae00169bab785ca
Mac Collins <macacollins@gmail.com>
2024-05-12 15:25:56 -0500

    Add missing test files.

---
+++ src/integrations/chess.com.test.js
@@ -0,0 +1,22 @@
+jest.mock('../storage');
+
+import refreshGames from './chess.com';
+
+import defaultGames from './default-games';
+
+
+// src/utils/currency.test.js
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({games: defaultGames}),
+  })
+);
+
+test('Parses chess.com mock data', async () => {
+    const setGames = jest.fn();
+    const playerName = "test";
+    const setSyncingGames = jest.fn();
+    const games = [];
+
+    await refreshGames(games, setGames, playerName, setSyncingGames);
+});
