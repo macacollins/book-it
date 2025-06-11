d5b8b020cbaf89dc84083cbbbb8fe2d49209a315
Mac Collins <macacollins@gmail.com>
2025-06-10 22:02:16 -0500

    First service worker push

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 
+import { join } from "node:path";
+import { buildSync } from "esbuild";
+
 export default defineConfig({
   plugins: [
-    react()],
+    react(),
+    {
+      apply: "build",
+      enforce: "post",
+      transformIndexHtml() {
+        buildSync({
+          minify: true,
+          bundle: true,
+          entryPoints: [join(process.cwd(), "src", "service-worker.js")],
+          outfile: join(process.cwd(), "src", "dist", "service-worker.js"),
+        });
+      },
+    },
+  ],
   base: './',
   server: {
     host: true
