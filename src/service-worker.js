2eb9c205a4704016fb324904a519baf882452c80
Mac Collins <macacollins@gmail.com>
2025-07-11 00:45:27 -0500

    Remove the last set of changes to service worker.

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,59 +8,70 @@ const filesToCache = [
   '/book-it'
 ];
 
-// the event handler for the activate event
-self.addEventListener('activate', e => {
-    console.debug("Hit activate event");
+const stuff = fetch("/book-it/assets/sources.json")
+    .then(a => a.json())
+    .then(sources => {
+        console.log("sources", sources)
 
-    try {
-        self.clients.claim()
-    } catch (e) {
-        console.debug("encountered exception during activate ", e);
-    }
-});
+        // the event handler for the activate event
+        self.addEventListener('activate', e => {
+            console.debug("Hit activate event");
 
-// the event handler for the install event 
-// typically used to cache assets
-self.addEventListener('install', e => {
-    console.log("Hit install event");
-    try {
-        e.waitUntil(
-            caches.open(cacheName)
-            .then(cache => cache.addAll(filesToCache))
-            );
+            try {
+                self.clients.claim()
+            } catch (e) {
+                console.debug("encountered exception during activate ", e);
+            }
+        });
 
-    } catch (e) {
-        console.log("Failed during install", e);
-    }
-    
+        // the event handler for the install event 
+        // typically used to cache assets
+        self.addEventListener('install', e => {
 
-});
+            const stuff = [...filesToCache, sources.css, sources.json];
+            console.log("Hit install event", stuff);
+
+            try {
+                e.waitUntil(
+                    caches.open(cacheName)
+                        .then(cache => cache.addAll(stuff))
+                    );
+
+            } catch (e) {
+                console.log("Failed during install", e);
+            }
+            
+
+        });
 
 
-async function cacheThenNetwork(request) {
-    try {
-        console.debug(request);
-        const cachedResponse = await caches.match(request);
-        if (cachedResponse) {
-          console.debug("Found response in cache:", cachedResponse);
-          return cachedResponse;
+        async function cacheThenNetwork(request) {
+            try {
+                console.debug(request);
+                const cachedResponse = await caches.match(request);
+                if (cachedResponse) {
+                console.debug("Found response in cache:", cachedResponse);
+                return cachedResponse;
+                }
+                console.debug("service worker cache miss")
+            } catch (e) {
+                console.debug("Got e.", e);
+            }
+
+            console.debug("Falling back to network");
+            return fetch(request);
         }
-        console.debug("service worker cache miss")
-    } catch (e) {
-        console.debug("Got e.", e);
-    }
+        
+        self.addEventListener("fetch", (event) => {
+            try {
+                console.debug(`Handling fetch event for ${event.request.url}`);
+                event.respondWith(cacheThenNetwork(event.request));
+            } catch (e) {
+                console.error(e);
+                // event.respondWith(fetch(request));
+            }
+        });
+        
+    });
 
-    console.debug("Falling back to network");
-    return fetch(request);
-  }
-  
-  self.addEventListener("fetch", (event) => {
-    try {
-        console.debug(`Handling fetch event for ${event.request.url}`);
-        event.respondWith(cacheThenNetwork(event.request));
-    } catch (e) {
-        console.error(e);
-        // event.respondWith(fetch(request));
-    }
-  });
-  
+console.log("Got stuff", stuff);
